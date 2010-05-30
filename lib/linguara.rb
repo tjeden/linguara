b531d5c1c03d92190a4a9f05864285c8ee284ccb
Aleksander Dabrowski <aleks@kumulator.pl>
2010-05-30 16:22:56 +0200

    serialize_form_data moved to utils

--- lib/linguara.rb
+++ lib/linguara.rb
@@ -4,6 +4,7 @@ require 'linguara/configuration'
 require 'linguara/utils'
 
 module Linguara
+  extend Linguara::Utils
   autoload :ActiveRecord, 'linguara/active_record'
   
   class << self
@@ -50,20 +51,6 @@ module Linguara
       end
     end
     
-    #TODO describe it, spec and move somewhere else
-     # http://www.keyongtech.com/5211204-nested-http-params-on-ruby
-    def serialize_form_data(data, path = "",serialized_params = [])
-       if data.kind_of? Hash
-         data.each_pair{|k,v| token = (path == "" ? url_encode(k) :  "[#{url_encode(k)}]"); serialize_form_data(v, "#{path}#{token}",serialized_params)}
-       elsif data.kind_of? Array
-         data.each{|v| serialize_form_data(v, "#{path}[]", serialized_params) }
-       else
-         #end of recursion
-         serialized_params << "#{path}=#{url_encode(data)}"
-       end
-
-       return serialized_params.join("&") if (path == "")
-     end
   end
 end
 
--- lib/linguara/configuration.rb
+++ lib/linguara/configuration.rb
@@ -1,7 +1,5 @@
 module Linguara
   class Configuration
-    OPTIONS = [:api_key, :server_path]
-    
     attr_accessor :api_key, :server_path, :return_url, :user, :password
     
     def initialize
