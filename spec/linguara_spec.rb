ecabb5b51c45be2c7095a7128c884cf67927def5
Aleksander Dabrowski <aleks@kumulator.pl>
2010-06-05 15:18:57 +0200

    simple spec for accept translation added

--- spec/linguara_spec.rb
+++ spec/linguara_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Linguara" do
+  
   it "should sent message to linguara for translatable classes" do
     FakeWeb.register_uri(:post, 'http://www.example.com/', :exception => Net::HTTPError)  
     blog_post = BlogPost.new( :title => "Title text", :body => "Body text")
@@ -8,6 +9,27 @@ describe "Linguara" do
       blog_post.save
     }.should raise_error(Net::HTTPError)
   end
+  
+  it 'should accept correct translation' do
+     FakeWeb.register_uri(:post, 'http://www.example.com/', :body => 'response_for_linguara', :status => 200)  
+     blog_post = BlogPost.new( :title => "Old title", :body => "Old body")
+     blog_post.save
+     id = blog_post.id
+     lambda {
+       Linguara.accept_translation(            
+            :paragraph => {
+             "BlogPost_#{id}_0_title"=> { :id =>"BlogPost_#{id}_0_title", :content =>"Hello World!"},
+             "BlogPost_#{id}_1_body"=> { :id =>"BlogPost_#{id}_1_body", :content =>"Today is great weather, and I am going to EuRuKo."}
+            },
+           :source_language =>"pl", 
+           :target_language =>"en"
+       )
+     }.should change(BlogPost, :count).by(0)
+     translation = BlogPost.last
+     translation.title.should eql("Hello World!")
+     translation.body.should eql("Today is great weather, and I am going to EuRuKo.")
+  end
+
 end
 
 describe 'Linguara::ActiveRecord' do
@@ -20,3 +42,4 @@ describe 'Linguara::ActiveRecord' do
     fields_to_send.last[1][:content].should eql("Body text")
   end
 end
+
